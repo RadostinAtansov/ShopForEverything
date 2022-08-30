9eaf84a8b6b7e414d547979744835c9138d9c3cf
Radostin Atanasov <hapa_21@abv.bg>
2022-08-30 20:54:36 +0300

    Add project files.

---
+++ ShopForEverything/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopForEverything.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
